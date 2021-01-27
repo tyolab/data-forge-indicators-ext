f1af3180469698ce6b83dc2335a91c8f663176bc
Eric Tang <eric@tyo.com.au>
2021-01-27 17:35:09 +1000

    init import

---
+++ README.md
@@ -0,0 +1,3 @@
+# data-forge-indicators-ext
+
+This is for the extension of [data forge indicators](https://data-forge.github.io/data-forge-indicators/).
---
+++ src/indicators/zigzag_pp.ts
@@ -0,0 +1,31 @@
+import { assert } from 'chai';
+import { ISeries, Series } from 'data-forge';
+
+export interface IZigZag_PP {
+    /**
+     * "hh", "hl", "lh", "ll"
+     */
+    point: string;
+}
+
+declare module "data-forge/build/lib/series" {
+    interface ISeries<IndexT, ValueT> {
+        zigzag_pp(period: number): ISeries<IndexT, number>;
+    }
+
+    interface Series<IndexT, ValueT> {
+        zigzag_pp(period: number): ISeries<IndexT, number>;
+    }
+}
+
+function zigzag_pp<IndexT = any>(this: ISeries<IndexT, number>, period: number): ISeries<IndexT, number> {
+
+	assert.isNumber(period, "Expected 'period' parameter to 'Series.sma' to be a number that specifies the time period of the moving average.");
+
+    return this.rollingWindow(period)
+        .select<[IndexT, number]>(window => [window.getIndex().last(), window.average()])
+        .withIndex(pair => pair[0])
+        .select(pair => pair[1]);
+}
+
+Series.prototype.zigzag_pp = zigzag_pp;
