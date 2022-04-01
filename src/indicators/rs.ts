eda75289ea1ebfe7ad033926b2ded4c4c4417852
Eric Tang <eric@tyo.com.au>
2022-04-02 08:20:05 +1000

    missed out changes

--- src/indicators/rs.ts
+++ src/indicators/rs.ts
@@ -56,32 +56,25 @@ function rs<IndexT = any>(this: ISeries<IndexT, number>, comparative: ISeries<In
         .select(pair1 => pair1[1]);
 }
 
-function rs_update<IndexT = any>(this: IDataFrame<IndexT, number>, comparative: ISeries<IndexT, number>, length: number, key: string, valueKey: string): IDataFrame<IndexT, number> {
+function rs_update<IndexT = any>(this: IDataFrame<number, any>, comparative: IDataFrame<number, any>, period: number, key: string, valueKey: string): IDataFrame<number, number> {
 
     if (!comparative) {
 
         return this;
     }
-    assert.equal(this.count(), comparative.count(), "The two series much contain same number of values");
+    assert.equal(this.count(), comparative.count(), "The two data frames much contain same number of values");
 
-    // as we can do the first day's range
-   return this.rollingWindow(length)
-        .select<[IndexT, number]>((window) => {
-            const day1 = window.first();
-            const day2 = window.last();
-            const index1 = window.getIndex().first();
-            const index2 = window.getIndex().last();
-            const c1 = comparative.at(index1) || 1;
-            const c2 = comparative.at(index2) || 1;
+    let pos: number = this.count() - period;
+    key = key || 'atr';
 
-            // @ts-ignore
-            return [
-                window.getIndex().last(),
-                (day2 / day1) / (c2 / c1) -1
-            ];
-        })
-        .withIndex(pair1 => pair1[0])
-        .select(pair1 => pair1[1]);
+    for (let i = pos; i < this.count(); ++i) {
+        let last_pos = i - period;
+        let window = this.between(last_pos, i).getSeries(valueKey);
+        let row = this.at(i);
+        row[key] = computeATR(lastRow, row);
+
+    }
+    return this;
 }
 
 Series.prototype.rs = rs;
