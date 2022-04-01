1b57e5d112fec8fde451cf2433a00a8722390370
Eric Tang <eric@tyo.com.au>
2022-04-01 17:22:02 +1000

    finished the update function for RS indicator

--- src/indicators/rs.ts
+++ src/indicators/rs.ts
@@ -20,18 +20,25 @@ declare module "data-forge/build/lib/series" {
 
 declare module "data-forge/build/lib/dataframe" {
     interface IDataFrame<IndexT, ValueT> {
-        rs_update(comparative: IDataFrame<IndexT, number>, length: number, key: string, valueKey: string): IDataFrame<IndexT, any>;
+        rs_update(comparative: IDataFrame<number, any>, length: number, update_period: number, key?: string, valueKey?: string): IDataFrame<IndexT, any>;
     }
 
     interface DataFrame<IndexT, ValueT> {
-        rs_update(comparative: IDataFrame<IndexT, any>, length: number, key: string, valueKey: string): IDataFrame<IndexT, any>;
+        rs_update(comparative: IDataFrame<number, any>, length: number, update_period: number, key?: string, valueKey?: string): IDataFrame<IndexT, any>;
     }
+}
+
+function computeRS<IndexT = any>(window: ISeries<IndexT, any>, comparative: ISeries<IndexT, any>) {
+    const day1 = window.first();
+    const day2 = window.last();
+    const c1 = comparative.first() || 1;
+    const c2 = comparative.last() || 1;
+    return (day2 / day1) / (c2 / c1) -1;
 }
 
 function rs<IndexT = any>(this: ISeries<IndexT, number>, comparative: ISeries<IndexT, number>, length: number = 50): ISeries<IndexT, number> {
 
     if (!comparative) {
-
         return this;
     }
     assert.equal(this.count(), comparative.count(), "The two series much contain same number of values");
@@ -39,49 +46,40 @@ function rs<IndexT = any>(this: ISeries<IndexT, number>, comparative: ISeries<In
     // as we can do the first day's range
    return this.rollingWindow(length)
         .select<[IndexT, number]>((window) => {
-            const day1 = window.first();
-            const day2 = window.last();
             const index1 = window.getIndex().first();
             const index2 = window.getIndex().last();
-            const c1 = comparative.at(index1) || 1;
-            const c2 = comparative.at(index2) || 1;
-
+            let window2 = comparative.between(index1, index2);
             // @ts-ignore
             return [
                 window.getIndex().last(),
-                (day2 / day1) / (c2 / c1) -1
+                computeRS(window, window2)
             ];
         })
         .withIndex(pair1 => pair1[0])
         .select(pair1 => pair1[1]);
 }
 
-function rs_update<IndexT = any>(this: IDataFrame<IndexT, number>, comparative: ISeries<IndexT, number>, length: number, key: string, valueKey: string): IDataFrame<IndexT, number> {
+function rs_update<IndexT = number>(this: IDataFrame<number, any>, comparative: IDataFrame<number, any>, period: number, update_period: number = 1, key?: string, valueKey?: string): IDataFrame<number, any> {
 
     if (!comparative) {
-
         return this;
     }
     assert.equal(this.count(), comparative.count(), "The two series much contain same number of values");
 
-    // as we can do the first day's range
-   return this.rollingWindow(length)
-        .select<[IndexT, number]>((window) => {
-            const day1 = window.first();
-            const day2 = window.last();
-            const index1 = window.getIndex().first();
-            const index2 = window.getIndex().last();
-            const c1 = comparative.at(index1) || 1;
-            const c2 = comparative.at(index2) || 1;
+    let pos: number = this.count() - update_period;
+    key = key || 'rs';
+    valueKey = valueKey || 'close';
 
-            // @ts-ignore
-            return [
-                window.getIndex().last(),
-                (day2 / day1) / (c2 / c1) -1
-            ];
-        })
-        .withIndex(pair1 => pair1[0])
-        .select(pair1 => pair1[1]);
+    for (let i = pos; i < this.count(); ++i) {
+        let last_pos = i - period;
+        let window = this.between(last_pos, i).getSeries(valueKey);
+        let window2 = comparative.between(last_pos, i).getSeries(valueKey); 
+        let row = this.at(i);
+        const value = computeRS(window, window2);
+        row[key] = value;
+    }
+
+    return this;
 }
 
 Series.prototype.rs = rs;
